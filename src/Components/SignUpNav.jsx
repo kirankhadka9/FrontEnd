02df453569384673b2e4b8b1d035febce3e46dd3
Kiran Khadka <kirankhadka@KIRANS-MACBOOK.local>
2024-03-01 21:48:16 +0545

    first

---
+++ src/Components/SignUpNav.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const SignUpNav = () => {
+  return (
+    <div style={{ display: "flex", justifyContent: "flex-end", alignItems: "center" }}>
+      <NavLink
+        to="/admin/login/sign-up"
+        style={{
+          marginRight: "30px", 
+          borderRadius: "5px", 
+          background: "#1d4ed8", 
+          color: "#ffffff", 
+          padding: "10px",
+          textDecoration: "none", 
+          textAlign: "center",
+          boxSizing: "border-box", 
+          display: "inline-block",
+        }}
+      >
+        Login
+      </NavLink>
+
+      <NavLink
+        to="/admin/sign-up/register"
+        style={{
+          marginRight: "30px", // Adjust as needed
+          borderRadius: "5px", // Adjust as needed
+          background: "#1d4ed8", 
+          color: "#ffffff", // Text color
+          padding: "10px",
+          textDecoration: "none", 
+          textAlign: "center",
+          boxSizing: "border-box", 
+          display: "inline-block",
+        }}
+      >
+        Register
+      </NavLink>
+    </div>
+  );
+};
+
+export default SignUpNav;
